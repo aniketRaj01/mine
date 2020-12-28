762b759bd6280a3345c3a81bf6fc7fad297ac34f
aniketRaj01 <aniketraj508.pe19je0133@pe.iitism.ac.in>
2020-12-28 09:57:07 -0800

    Updates

---
+++ precache-manifest.9c515fce3d84b73ad301f705d077ad18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8f6fb082137c07274213d3cdd532b7",
+    "url": "/mine/index.html"
+  },
+  {
+    "revision": "38f988cacf4814eece0b",
+    "url": "/mine/static/css/2.363f5bca.chunk.css"
+  },
+  {
+    "revision": "1f2b5297d72aeaad91cb",
+    "url": "/mine/static/css/main.9402aac8.chunk.css"
+  },
+  {
+    "revision": "38f988cacf4814eece0b",
+    "url": "/mine/static/js/2.35daada7.chunk.js"
+  },
+  {
+    "revision": "907a749a319772c60fb75dc74809d42c",
+    "url": "/mine/static/js/2.35daada7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2b5297d72aeaad91cb",
+    "url": "/mine/static/js/main.66885720.chunk.js"
+  },
+  {
+    "revision": "bd466e562ac1a1266fe3",
+    "url": "/mine/static/js/runtime-main.975ab53c.js"
+  },
+  {
+    "revision": "eabb53bf44fbc87bc691ad7c6e0d0bc2",
+    "url": "/mine/static/media/aboutus.eabb53bf.jpg"
+  },
+  {
+    "revision": "c9b4beaef16e10a36e6978a3faf8b9f9",
+    "url": "/mine/static/media/logo-white.c9b4beae.png"
+  }
+]);
